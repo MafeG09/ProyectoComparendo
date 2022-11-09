253f901f5e377405718f48f1e84275953313218d
MafeG09 <mariaf.camachog@uqvirtual.edu.co>
2022-11-08 19:52:18 -0500

    Fixed index controller

--- web/src/main/java/co/edu/uniquindio/comparendo/IndexController.java
+++ web/src/main/java/co/edu/uniquindio/comparendo/IndexController.java
@@ -2,8 +2,10 @@ package co.edu.uniquindio.comparendo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/")
+@RestController
 public class IndexController {
 
     @GetMapping
